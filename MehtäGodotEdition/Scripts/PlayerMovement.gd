d9affe5930f40105ac71e07149caaf2187db930d
lukabra <lukabrannlund@kamk.fi>
2024-06-05 23:22:50 +0300

    _

--- MehtäGodotEdition/Scripts/PlayerMovement.gd
+++ MehtäGodotEdition/Scripts/PlayerMovement.gd
@@ -1,5 +1,12 @@
 extends Node
 
+##bumpers
+@export var rightBumper: Area2D
+@export var leftBumper: Area2D
+@export var upBumper: Area2D
+@export var downBumper: Area2D
+
+##movement
 @export var movementSpeed: float
 @export var acceleration: float
 @export var movementEnabled: bool
@@ -12,7 +19,8 @@ var tick: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var parent = get_parent() as CharacterBody2D
+	
+	
 	pass # Replace with function body.
 
 
@@ -22,34 +30,47 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
+	
+	Movement(delta)
+	
+	pass
+	
+func Movement(delta):
 	var parent = get_parent() as CharacterBody2D
 	var input: Vector2 
 	var motion: Vector2
 	
 	movingDirection = (parent.global_position - lastFramePosition).normalized()
 	
-	print(parent.global_position)
+	var right = Input.get_action_raw_strength("MoveRight")
+	var left = Input.get_action_raw_strength("MoveLeft")
+	var down = Input.get_action_raw_strength("MoveDown")
+	var up = Input.get_action_raw_strength("MoveUp")
 	
-	input.x = Input.get_action_raw_strength("MoveRight") - Input.get_action_raw_strength("MoveLeft")
-	input.y = Input.get_action_raw_strength("MoveDown") - Input.get_action_raw_strength("MoveUp")
+	if rightBumper.has_overlapping_bodies():
+		right = 0
+	if leftBumper.has_overlapping_bodies():
+		left = 0
+	if upBumper.has_overlapping_bodies():
+		up = 0
+	if downBumper.has_overlapping_bodies():
+		down = 0
+	
+	input.x = right - left
+	input.y = down - up
 	
 	movementSlope = clamp(movementSlope + (acceleration * ((clamp(abs(input.x) + abs(input.y),0.0,1.0)*2)-1)), 0.0, 1.0)
 	
-	if (abs(input.x) + abs(input.y)) <= 0:
-		motion = movingDirection
-	else:
-		motion = input.normalized()
+	parent.velocity = input * delta * movementSpeed * movementSlope * 100
+	parent.move_and_collide(parent.velocity)
+			
 	
-	if movementEnabled:
-		var collision = parent.move_and_collide(motion * movementSlope * movementSpeed * 250 * delta)
-		if collision:
-			parent.velocity = parent.velocity.slide(collision.get_normal())
-		
-	if tick > 3:
+	if tick > 1:
 		tick = 0
 		lastFramePosition = parent.global_position
 	else:
 		tick = tick + 1
+	
 	pass
 
 
